6749da105e68f17b31b1315d894874e674e1f867
Michał Adamczyk <michal.lukasz.adamczyk@gmail.com>
2025-02-05 15:01:36 +0200

    Emacs: add 'helpful'

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -175,3 +175,12 @@
   (setq ediff-keep-variants nil
         ediff-split-window-function 'split-window-horizontally
         ediff-window-setup-function 'ediff-setup-windows-plain))
+
+(use-package helpful
+  :config
+  (global-set-key (kbd "C-h f") #'helpful-callable)
+  (global-set-key (kbd "C-h v") #'helpful-variable)
+  (global-set-key (kbd "C-h k") #'helpful-key)
+  (global-set-key (kbd "C-h x") #'helpful-command)
+  (global-set-key (kbd "C-c C-d") #'helpful-at-point)
+  (global-set-key (kbd "C-h F") #'helpful-function))
