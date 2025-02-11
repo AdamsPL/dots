520e6812f9f8ff6773aa28f3e0153a5877170ae2
Michał Adamczyk <michal.lukasz.adamczyk@gmail.com>
2025-02-11 16:52:24 +0100

    Emacs: switch to straight.el

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,10 +1,25 @@
 ;; -*- lexical-binding:t -*-
 
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-(setq package-native-compile t)
+(straight-use-package 'use-package)
+(setq straight-vc-git-default-clone-depth 1)
+(setq straight-use-package-by-default t)
+
 (setq native-comp-async-query-on-exit t)
 (setq confirm-kill-processes t)
 (setq-default tab-width 4)
@@ -12,6 +27,7 @@
 (setq use-short-answers t)
 (setq completion-styles '(basic substring flex))
 (setq read-extended-command-predicate #'command-completion-default-include-p)
+
 (setq vc-follow-symlinks t)
 (setq major-mode-remap-alist
     '((python-mode . python-ts-mode)
@@ -19,6 +35,7 @@
         (c++-mode . c++-ts-mode)
         (bash-mode . bash-ts-mode)
         (rust-mode . rust-ts-mode)))
+
 (setq isearch-wrap-pause 'no)
 (setq split-height-threshold 200)
 (toggle-frame-maximized)
